6831dd72baaf85a79e0d7d8b04645a4e00c02733
Lloyd1020 <lloydemerson.lim.cics@ust.edu.ph>
2026-02-05 05:22:20 +0800

    Break the test on purpose

--- server.test.js
+++ server.test.js
@@ -1,6 +1,6 @@
 // A simple test to check if math works
 test('1 + 1 should equal 2', () => {
-    expect(1 + 1).toBe(2);
+    expect(1 + 1).toBe(5);
 });
 
 // A dummy test to check if the app name is correct
