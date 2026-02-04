5a83a99bea5899e0d4ea9ffcad4769d7caa27f1c
Lloyd1020 <lloydemerson.lim.cics@ust.edu.ph>
2026-02-05 05:12:50 +0800

    Add Jest Testing

---
+++ server.test.js
@@ -0,0 +1,10 @@
+// A simple test to check if math works
+test('1 + 1 should equal 2', () => {
+    expect(1 + 1).toBe(2);
+});
+
+// A dummy test to check if the app name is correct
+test('App name should be Backend', () => {
+    const appName = "Backend";
+    expect(appName).toBe("Backend");
+});
